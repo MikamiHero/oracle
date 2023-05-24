9e3491a58e6b8b99e5bc4fe7c411ffb04d87e00a
MikamiHero <21453519+MikamiHero@users.noreply.github.com>
2023-05-24 17:04:34 +1000

    Committing the stock config.json

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 `oracle` is a Discord helper written in C using the `Concord` Discord API library.
 
+## Installing dependencies
+The main dependency is `Concord`. You can install it by following the instructions [here](https://github.com/Cogmasters/concord#installing-concord). 
+
+## Building and running
+From the root of the project, type the following to build and run `oracle`
+```shell
+$ make
+$ ./oracle
+```
+
+If you make changes to anything in `src/`, then run `make clean` before building.
+
 ## TODO:
 
 - Set up bot to join channel
--- src/bot.c
+++ src/bot.c
@@ -18,5 +18,21 @@ on_ready(struct discord *client, const struct discord_ready *event)
 int
 main(int argc, char *argv[])
 {
-    return 0;
+    // Reading in the config file
+    const char *config_file;
+    if (argc > 1)
+        config_file = argv[1];
+    else
+        config_file = "./config.json";
+
+    // Initialise bot
+    ccord_global_init();
+    struct discord *client = discord_config_init(config_file);
+    assert(NULL != client && "Could not initialize client");
+
+    discord_set_on_ready(client, &on_ready);
+    discord_run(client);
+
+    discord_cleanup(client);
+    ccord_global_cleanup();
 }
